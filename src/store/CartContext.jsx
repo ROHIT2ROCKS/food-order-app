b822fa18bcdc931af2b0246999fdb9ba8c8eb779
Rohit Lakhani <rohitlakhani1999@gmail.com>
2025-11-11 11:20:12 +0530

    everything working

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: ()=> {}
 });
 
 function cartReducer(state, action) {
@@ -54,6 +55,10 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
+  if (action.type === 'CLEAR_CART') {
+    return {...state, items: []}
+  }
+
   return state;
 }
 
@@ -67,11 +72,16 @@ export function CartContextProvider({ children }) {
     function removeItem(id) {
         dispatchCartAction({type: "REMOVE_ITEM", id})
     }
+
+    function clearCart() {
+      dispatchCartAction({type: 'CLEAR_CART'})
+    }
     
     const cartContext = {
       items: cart.items,
       addItem,
-      removeItem
+      removeItem,
+      clearCart
     }
 
     console.log(cartContext)
